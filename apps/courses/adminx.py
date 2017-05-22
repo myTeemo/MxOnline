d4ab03e41013c5419f303b2c2a5449cf57c375f7
myTeemo <mingyang.hey@gmail.com>
2017-05-22 22:38:29 +0800

    添加xadmin 识别的adminx.py文件，此文件用户课程model的注册

---
+++ apps/courses/adminx.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import xadmin
+
+from .models import Course
+from .models import Lesson
+from .models import Video
+from .models import CourseResource
+
+
+__author__ = "Eilene HE"
+__date__ = '17/5/22 13:38'
+
+
+class CourseAdmin(object):
+    list_display = ['name','desc','detail','degree','learn_times','students','fav_nums','image','click','add_time']
+    search_fields = ['name','degree','learn_times','students','fav_nums','click']
+    list_filter = ['name','degree','learn_times','students','fav_nums','click','add_time']
+
+
+class LessonAdmin(object):
+    list_display = ['course','name','add_time']
+    search_fields = ['course','name']
+    list_filter = ['course__name','name','add_time']
+
+
+class VideoAdmin(object):
+    list_display = ['lesson','name','add_time']
+    search_fields = ['lesson','name']
+    list_filter = ['lesson__name','name','add_time']
+
+
+class CourseResourceAdmin(object):
+    list_display = ['course','name','download','add_time']
+    search_fields = ['course','name']
+    list_filter = ['course__name','name']
+
+
+xadmin.site.register(Course,CourseAdmin)
+xadmin.site.register(Lesson,LessonAdmin)
+xadmin.site.register(Video,VideoAdmin)
+xadmin.site.register(CourseResource,CourseResourceAdmin)
