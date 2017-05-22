40149d88f3ea0358ebba9b3c1926508741d66ed6
myTeemo <mingyang.hey@gmail.com>
2017-05-22 22:47:21 +0800

    修改应用在xadmin左侧的菜单显示名称

--- apps/courses/apps.py
+++ apps/courses/apps.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
@@ -5,3 +6,4 @@ from django.apps import AppConfig
 
 class CoursesConfig(AppConfig):
     name = 'courses'
+    verbose_name = '课程管理'
