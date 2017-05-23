eb15c569d23c280d719045e60df7a987991e85d3
myTeemo <mingyang.hey@gmail.com>
2017-05-23 23:51:36 +0800

    添加登录逻辑和重载登录逻辑使之可以邮箱登录

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,35 @@
+# -*- coding:utf-8 -*-
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q
 
-# Create your views here.
+from .models import UserProfile
+
+
+class CustomBackend(ModelBackend):
+    def authenticate(self, username=None, password=None, **kwargs):
+        try:
+            user = UserProfile.objects.get(Q(username=username)|Q(email=username))
+            if user.check_password(password):
+                return user
+        except Exception as e:
+            return None
+
+
+def user_login(request):
+    if request.method == "POST":
+        user_name = request.POST.get('username','')
+        pass_word = request.POST.get('password','')
+        user = authenticate(username=user_name,password=pass_word)
+
+        if user is not None:
+            login(request,user)
+            return render(request,"index.html",{})
+        else:
+            return render(request,"login.html",{'msg':u"用户名或密码错误"})
+
+    elif request.method == "GET":
+
+        return render(request,"login.html",{})
