60bbf53c800415987547fd6391d074e8da5a6383
myTeemo <mingyang.hey@gmail.com>
2017-05-23 23:50:09 +0800

    增加首页和登录路由

--- MxOnline/urls.py
+++ MxOnline/urls.py
@@ -14,11 +14,17 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+import xadmin
+
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
-import xadmin
+from users.views import user_login
+
 
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
+    url(r'^$',TemplateView.as_view(template_name="index.html"),name="index"),
+    url(r'^login/$',user_login,name="login"),
 ]
