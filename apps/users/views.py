ec48826399b3a3abf8b345883b9ed2bf40cc3ff9
myTeemo <mingyang.hey@gmail.com>
2017-05-24 23:28:44 +0800

    添加基于类的方式登录，增加表单验证

--- apps/users/views.py
+++ apps/users/views.py
@@ -4,8 +4,10 @@ from django.contrib.auth import authenticate
 from django.contrib.auth import login
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
+from django.views.generic.base import View
 
 from .models import UserProfile
+from .forms import LoginForm
 
 
 class CustomBackend(ModelBackend):
@@ -16,6 +18,26 @@ class CustomBackend(ModelBackend):
                 return user
         except Exception as e:
             return None
+
+
+class LoginView(View):
+    def get(self,request):
+        return render(request,"login.html",{})
+
+    def post(self,request):
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            pass
+            user_name = request.POST.get('username','')
+            pass_word = request.POST.get('password','')
+            user = authenticate(username=user_name,password=pass_word)
+            if user is not None:
+                login(request,user)
+                return render(request,'index.html',)
+            else:
+                return render(request,'login.html',{'msg':'用户名或密码错误'})
+        else:
+            return render(request,'login.html',{'login_form':login_form})
 
 
 def user_login(request):
