fa94b32b3b0b3d57ffd1eb7e3cead747545c15b9
myTeemo <mingyang.hey@gmail.com>
2017-05-26 22:43:28 +0800

    添加发送邮件类

---
+++ apps/utils/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Eilene HE"
+__date__ = '17/5/25 23:26'
---
+++ apps/utils/email_send.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from random import Random
+
+from django.core.mail import send_mail
+from MxOnline.settings import EMAIL_FROM
+
+from users.models import EmailVerifyRecord
+__author__ = "Eilene HE"
+__date__ = '17/5/25 23:27'
+
+
+def random_str(random_length = 8):
+    str = ''
+    chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz123456890'
+    length = len(chars)-1
+    random = Random()
+    for i in range(random_length):
+        str += chars[random.randint(0,length)]
+    return str
+
+
+def send_register_email(email, send_type = 'register'):
+    email_record = EmailVerifyRecord()
+    code = random_str(16)
+    email_record.code = code
+    email_record.send_type = send_type
+    email_record.save()
+
+    email_title = ''
+    email_body = ''
+    if send_type == 'register':
+        email_title = '幕学在线网注册激活链接'
+        email_body = '请点击下面的连接激活你的账号: http://127.0.0.1/active/{0}'.format(code)
+
+        send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
+        if send_status:
+            pass
+
