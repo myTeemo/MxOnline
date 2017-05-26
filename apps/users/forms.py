19d4ab7236de50080beebaae4539b9e5ff7bc8c3
myTeemo <mingyang.hey@gmail.com>
2017-05-26 22:46:00 +0800

    添加注册表单逻辑

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django import forms
+from captcha.fields import CaptchaField
 
 __author__ = "Eilene HE"
 __date__ = '17/5/24 22:45'
@@ -8,4 +9,10 @@ __date__ = '17/5/24 22:45'
 class LoginForm(forms.Form):
     username = forms.CharField(required=True)
     password = forms.CharField(required=True,min_length=6)
+
+
+class RegisterForm(forms.Form):
+    email = forms.EmailField(required=True)
+    password = forms.CharField(required=True,min_length=6)
+    captcha = CaptchaField(error_messages={"invalid":u'验证码错误'})
 
