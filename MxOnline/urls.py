21e9326cc3e297a81ee9fc8973d1fc8a80f3abbd
myTeemo <mingyang.hey@gmail.com>
2017-05-26 22:45:07 +0800

    配置注册和验证码的url

--- MxOnline/urls.py
+++ MxOnline/urls.py
@@ -17,16 +17,20 @@ Including another URLconf
 import xadmin
 
 from django.conf.urls import url
+from django.conf.urls import include
 from django.contrib import admin
 from django.views.generic import TemplateView
 
 from users.views import user_login
 from users.views import LoginView
-
+from users.views import RegisterView
 
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
     url(r'^$',TemplateView.as_view(template_name="index.html"),name="index"),
     # url(r'^login/$',user_login,name="login"),
-    url(r'login/$',LoginView.as_view(),name='login')
+    url(r'^captcha/',include('captcha.urls')),
+    url(r'login/$',LoginView.as_view(),name='login'),
+    url(r'register/$',RegisterView.as_view(),name='register')
+
 ]
