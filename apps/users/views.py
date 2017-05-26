529f84e06ab7a5f034be10bb6bc8585b3cb1d535
myTeemo <mingyang.hey@gmail.com>
2017-05-26 22:46:37 +0800

    添加注册处理逻辑

--- apps/users/views.py
+++ apps/users/views.py
@@ -5,10 +5,12 @@ from django.contrib.auth import login
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
 from django.views.generic.base import View
+from django.contrib.auth.hashers import make_password
 
 from .models import UserProfile
 from .forms import LoginForm
-
+from .forms import RegisterForm
+from utils.email_send import send_register_email
 
 class CustomBackend(ModelBackend):
     def authenticate(self, username=None, password=None, **kwargs):
@@ -18,6 +20,25 @@ class CustomBackend(ModelBackend):
                 return user
         except Exception as e:
             return None
+
+
+class RegisterView(View):
+    def get(self,request):
+        register_form = RegisterForm()
+        return render(request,'register.html',{'register_form':register_form})
+
+    def post(self,request):
+        register_form = RegisterForm(request.POST)
+        if register_form.is_valid():
+            user_name = request.POST.get('email','')
+            pass_word = request.POST.get('password','')
+            user_profile = UserProfile()
+            user_profile.username = user_name
+            user_profile.email = user_name
+            user_profile.password = make_password(pass_word)
+            user_profile.save()
+
+            send_register_email(user_name,'register')
 
 
 class LoginView(View):
