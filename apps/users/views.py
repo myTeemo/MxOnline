b1ac038011eef05fa67894d084211e09391ac318
myTeemo <mingyang.hey@gmail.com>
2017-05-27 23:44:55 +0800

    完善注册功能存在的缺陷，添加重置密码的功能

--- apps/users/views.py
+++ apps/users/views.py
@@ -11,7 +11,10 @@ from .models import UserProfile
 from .models import EmailVerifyRecord
 from .forms import LoginForm
 from .forms import RegisterForm
+from .forms import ForgetForm
+from .forms import ModifyPwdForm
 from utils.email_send import send_register_email
+
 
 class CustomBackend(ModelBackend):
     def authenticate(self, username=None, password=None, **kwargs):
@@ -32,7 +35,38 @@ class ActiveUserView(View):
                 user = UserProfile.objects.get(email=email)
                 user.is_active = True
                 user.save()
+        else:
+            return render(request,'active_file.html',{})
+
         return render(request,'login.html',{})
+
+
+class ResetView(View):
+    def get(self,request,reset_code):
+        all_records = EmailVerifyRecord.objects.filter(code=reset_code)
+        if all_records:
+            for record in all_records:
+                email = record.email
+                return render(request,'password_reset.html',{'email':email})
+
+
+class ModifyPwdView(View):
+    def post(self,request):
+        modify_form = ModifyPwdForm(request.POST)
+        if modify_form.is_valid():
+            password1 = request.POST.get('password1','')
+            password2 = request.POST.get('password2','')
+            email = request.POST.get('email','')
+            if password1 != password2:
+                return render(request,'password_reset.html',{'email':email,'msg':u'密码不一致'})
+            user = UserProfile.objects.get(email=email)
+            user.password = make_password(password2)
+            user.save()
+            return render(request,'login.html')
+        else:
+            email = request.POST.get('email','')
+
+            return render(request,'password_reset.html',{'email': email, 'modify_form': modify_form})
 
 
 class RegisterView(View):
@@ -44,6 +78,8 @@ class RegisterView(View):
         register_form = RegisterForm(request.POST)
         if register_form.is_valid():
             user_name = request.POST.get('email','')
+            if UserProfile.objects.filter(email=user_name):
+                return render(request,'register.html',{'register_form':register_form, 'msg':'用户已经存在'})
             pass_word = request.POST.get('password','')
             user_profile = UserProfile()
             user_profile.username = user_name
@@ -79,21 +115,18 @@ class LoginView(View):
             return render(request,'login.html',{'login_form':login_form})
 
 
-def user_login(request):
-    if request.method == "POST":
-        user_name = request.POST.get('username','')
-        pass_word = request.POST.get('password','')
-        user = authenticate(username=user_name,password=pass_word)
+class ForgetPwdView(View):
+    def get(self,request):
+        forget_form = ForgetForm()
+        return render(request,'forgetpwd.html',{'forget_form':forget_form})
 
-        if user is not None:
-            if user.is_active:
-                login(request,user)
-                return render(request,"index.html",{})
-            else:
-                return render(request,'login.html',{'msg':u'用户未激活'})
+    def post(self,request):
+        forget_form = ForgetForm(request.POST)
+        if forget_form.is_valid():
+            email = request.POST.get('email','')
+            send_register_email(email,'forget')
+
+            return render(request,'send_success.html',{})
         else:
-            return render(request,"login.html",{'msg':u"用户名或密码错误"})
+            return render(request,'forgetpwd.html',{'forget_form':forget_form})
 
-    elif request.method == "GET":
-
-        return render(request,"login.html",{})
